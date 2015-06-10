399e4f91965fab7546386a10be33680698161a25
Ilian <ugg.rock@gmail.com>
2015-06-10 08:41:59 +0200

    Adding sendto functionality

--- www/fileopener.js
+++ www/fileopener.js
@@ -14,4 +14,17 @@ module.exports = {
         }
         cordova.exec(opt_success, opt_failure, "FileOpener", "openFile", [url]);
     }
+    send: function (url, opt_success, opt_failure) {
+        if (typeof opt_success == 'undefined') {
+            opt_success = function () {
+                console.log("success!");
+            }
+        }
+        if (typeof opt_failure == 'undefined') {
+            opt_failure = function (error) {
+                console.log(error);
+            }
+        }
+        cordova.exec(opt_success, opt_failure, "FileOpener", "sendFile", [url]);
+    }    
 }
